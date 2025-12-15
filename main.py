7e95243b029bc9ce227f699d1a78aab2b1b629d6
renbrodev <renbrodev@gmail.com>
2025-12-15 13:36:00 -0700

    ADD word count, character log, sorted character and helper functions
    Also a print report function. My bad on not updating git per feature.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from stats import get_word_count, get_char_log
+from stats import get_word_count, get_char_log, get_sorted_char_log
 
 def get_book_text(filepath):
+
     with open(filepath) as file:
         contents = file.read()
 
@@ -8,8 +9,21 @@ def get_book_text(filepath):
 
 def main():
     # print(get_book_text("books/frankenstein.txt"))
-    print(f"Found {get_word_count(get_book_text("books/frankenstein.txt"))} total words.")
-    print(f"Char Log: {get_char_log(get_book_text("books/frankenstein.txt"))}")
+    word_count = get_word_count(get_book_text("books/frankenstein.txt"))
+    char_log = get_char_log(get_book_text("books/frankenstein.txt"))
+    sorted_log = get_sorted_char_log(char_log)
 
+    print_report(sorted_log)
 
+def print_report(char_count_dict):
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_word_count(get_book_text("books/frankenstein.txt"))} total words")
+    print("--------- Character Count -------")
+    for dict in char_count_dict:
+        print(f"{dict["char"]}: {dict["num"]}")
+    print("============= END ===============")
+    
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,41 @@ def get_char_log(input_string):
             char_list[input_string[i].lower()] += 1
 
     return char_list
+
+def get_sorted_char_log(unsorted_log):
+    
+    log_list = [] # list for holding char num key pairs
+    char_count_list =[] # list for holding char count so we can sort
+    sorted_chars = [] #list that will hold sorted version
+
+    # create char num key pair list so we can fix new list in the end
+    for log in unsorted_log:
+        log_list.append({"char" : log, "num" : unsorted_log[log]})
+
+    # make the char list from the log list
+    for log in log_list:
+        char_count_list.append(log["num"])
+
+    # then sort it
+    char_count_list.sort(reverse=True)
+
+    sorted_chars = help_get_sorted_char_log(char_count_list, log_list)
+    
+    # pass the sorted char list and dictionary in to helper so it can be rearranged
+    return sorted_chars
+
+def help_get_sorted_char_log(num_list, char_dict):
+    sorted_dicts = []
+
+    # tedious but for each number check each entry in the dictionary
+    for num in num_list:
+        for dict in char_dict:
+            # if you find a match append it to the list
+            if dict["num"] == num:
+                # skip non alpha characters
+                if dict["char"].isalpha() == False:
+                    continue
+                sorted_dicts.append(dict)
+    
+    # since we are going down an ordered list of numbers to check we will end up with an ordered list of dictionaries
+    return sorted_dicts
